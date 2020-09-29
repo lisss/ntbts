358532c2afba32779ba51db5597ccb0e304bc226
lisss <alicedeadbride@gmail.com>
2020-09-29 23:38:08 +0300

    valid parens try

---
+++ data_structures/valid_parens.py
@@ -0,0 +1,25 @@
+class Solution:
+    def __init__(self):
+        self.stack = []
+
+    def isValid(self, s: str):
+        if not len(s):
+            return True
+        if len(s) % 2:
+            return False
+        for i, x in enumerate(s):
+            if not len(self.stack):
+                self.stack.append(x)
+            else:
+                prev_idx = len(self.stack) - 1
+                if self.stack[prev_idx] == '(' and x == ')':
+                    del self.stack[prev_idx]
+                elif self.stack[prev_idx] == '[' and x == ']':
+                    del self.stack[prev_idx]
+                elif self.stack[prev_idx] == '{' and x == '}':
+                    del self.stack[prev_idx]
+                else:
+                    self.stack.append(x)
+        if not len(self.stack):
+            return True
+        return False
