707dadbcba65fd51e1ef8d432bdc24ee73dbf404
lisss <alicedeadbride@gmail.com>
2020-10-11 11:35:33 +0300

    fix

--- data_structures/add_binary.py
+++ data_structures/add_binary.py
@@ -8,16 +8,17 @@ class Solution:
         len_diff = len(max_num) - len(min_num)
         start, end = 0, len(max_num) - 1
         res = ['0'] * (len(max_num) + 1)
+        mem = 0
         while start <= end:
             a_val = int(max_num[end])
             b_val = int(min_num[end - len_diff]) if end - len_diff >= 0 else 0
-            curr = a_val + b_val + int(res[end + 1])
+            curr = a_val + b_val + mem
             write = curr % BASE
             mem = curr // BASE
             res[end + 1] = str(write)
-            if mem > 0:
-                res[end] = str(mem)
             end -= 1
+        if mem:
+            res[0] = str(mem)
         if res[0] == '0':
             del res[0]
         return ''.join(res)
