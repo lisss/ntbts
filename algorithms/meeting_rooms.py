8a0ff456b42631dbcd47e9c980d51ca1ca94b381
lisss <alicedeadbride@gmail.com>
2020-11-20 21:47:38 +0200

    remove extra var

--- algorithms/meeting_rooms.py
+++ algorithms/meeting_rooms.py
@@ -7,17 +7,14 @@ class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]):
         intervals = sorted(intervals, key=lambda x: x[0])
         booked_clots = []
-        count = 0
 
         for start, end in intervals:
             if not len(booked_clots):
-                heapq.heappush(booked_clots, (end, count))
-                count += 1
+                heapq.heappush(booked_clots, end)
             else:
-                last_end, _ = heapq.nsmallest(1, booked_clots)[0]
+                last_end = heapq.nsmallest(1, booked_clots)[0]
                 if start >= last_end:
                     heapq.heappop(booked_clots)
-                heapq.heappush(booked_clots, (end, count))
-                count += 1
+                heapq.heappush(booked_clots, end)
 
         return len(booked_clots)
