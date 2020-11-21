d2e6dc54967c3327079dfdbbc21373bd20ab7f43
lisss <alicedeadbride@gmail.com>
2020-11-21 22:01:40 +0200

    palindrome with allowed to delete

--- algorithms/palindrome.py
+++ algorithms/palindrome.py
@@ -18,8 +18,8 @@ def is_num_palindrome(num: int):
     return True
 
 
-# Valid Palindrome
 class Solution:
+    # https://leetcode.com/problems/valid-palindrome/
     def isPalindrome(self, s: str):
         start, end = 0, len(s) - 1
 
@@ -32,6 +32,34 @@ class Solution:
                 continue
             if s[start].lower() != s[end].lower():
                 return False
+            start += 1
+            end -= 1
+
+        return True
+
+    # https://leetcode.com/problems/valid-palindrome-ii/
+
+    def check_palindrome(self, s, start, end):
+        while start < end:
+            if s[start] != s[end]:
+                return False
+            start += 1
+            end -= 1
+        return True
+
+    def validPalindrome(self, s: str):
+        start, end = 0, len(s) - 1
+        allowed_to_delete = True
+
+        while start < end:
+            ch_start, ch_end = s[start], s[end]
+            if ch_start != ch_end:
+                if allowed_to_delete:
+                    allowed_to_delete = False
+                    return self.check_palindrome(s, start + 1, end) \
+                        or self.check_palindrome(s, start, end - 1)
+                else:
+                    return False
             start += 1
             end -= 1
 
