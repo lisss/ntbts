0e8fa757aca1e1ff7cbeceacefea02a85218298f
lisss <alicedeadbride@gmail.com>
2020-11-30 23:57:16 +0200

    rename

--- algorithms/trees.py
+++ algorithms/trees.py
@@ -7,19 +7,19 @@ class TreeNode:
 
 class Solution:
     # https://leetcode.com/problems/binary-tree-inorder-traversal/
-    def _do(self, root: TreeNode, res):
+    def _do_in_order_traverse(self, root: TreeNode, res):
         if not root:
             return
         if root.left:
-            self._do(root.left, res)
+            self._do_in_order_traverse(root.left, res)
 
         res.append(root.val)
 
         if root.right:
-            self._do(root.right, res)
+            self._do_in_order_traverse(root.right, res)
 
         return res
 
     def inorderTraversal(self, root: TreeNode):
         res = []
-        return self._do(root, res)
+        return self._do_in_order_traverse(root, res)
