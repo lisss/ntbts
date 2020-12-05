64bfe224d846f199658588af406482567358ebed
lisss <alicedeadbride@gmail.com>
2020-12-05 15:21:57 +0200

    fix

--- algorithms/arrays.py
+++ algorithms/arrays.py
@@ -83,7 +83,7 @@ class Solution:
     def rotate(self, nums: List[int], k: int):
         copy = []
         start, end = 0, len(nums)
-        k = k if k <= len(nums) else k - len(nums)
+        k = k % len(nums)
         mid = len(nums) - k
 
         while mid < end:
