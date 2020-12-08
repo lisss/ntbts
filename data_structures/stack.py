80a3cd2c86c71eb866d55b447dd105d5ab47eaa9
lisss <alicedeadbride@gmail.com>
2020-12-09 00:32:28 +0200

    exclusive tim eof functions

--- data_structures/stack.py
+++ data_structures/stack.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class Stack:
     def __init__(self):
         self.arr = [None] * 4
@@ -21,6 +24,57 @@ class Stack:
 
     def is_empty(self):
         return self.empty
+
+
+# https://leetcode.com/problems/exclusive-time-of-functions/
+class CallStack:
+    def __init__(self, n):
+        self.n = n + 1
+        self.arr = [0] * self.n
+        self.last = -1
+
+    def push(self, val):
+        self.last += 1
+        if self.last == len(self.arr) - 1:
+            self.arr.extend([0] * self.n)
+        self.arr[self.last] = val
+
+    def pop(self):
+        last = self.arr[self.last]
+        self.last -= 1
+        return last
+
+    def is_empty(self):
+        return self.last == -1
+
+    def peek(self):
+        return self.arr[self.last] if self.last >= 0 else 0
+
+
+class CallStackSolution:
+    def exclusiveTime(self, n: int, logs: List[str]):
+        stack = CallStack(n)
+        res = [0] * n
+        prev = 0
+
+        for log in logs:
+            fn_id, event, time = log.split(':')
+            time = int(time)
+            fn_id = int(fn_id)
+
+            last = stack.peek()
+
+            if event == 'start':
+                if not stack.is_empty():
+                    res[last] += time - prev
+                stack.push(fn_id)
+                prev = time
+            else:
+                res[last] += time - prev + 1
+                stack.pop()
+                prev = time + 1
+
+        return res
 
 
 def main():
