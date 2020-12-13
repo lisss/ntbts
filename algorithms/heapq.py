46e7e3b195d63f356edc78768b35f72ac147028e
lisss <alicedeadbride@gmail.com>
2020-12-14 01:43:22 +0200

    k closest points

--- algorithms/heapq.py
+++ algorithms/heapq.py
@@ -18,3 +18,19 @@ class Solution:
                 heapq.heappush(booked_clots, end)
 
         return len(booked_clots)
+
+    def kClosest(self, points: List[List[int]], K: int):
+        def _get_dist(p):
+            return p[0] ** 2 + p[1] ** 2
+
+        res = []
+        final_res = []
+
+        for x in points:
+            heapq.heappush(res, (_get_dist(x), x))
+
+        for _ in range(K):
+            _, x = heapq.heappop(res)
+            final_res.append(x)
+
+        return final_res
